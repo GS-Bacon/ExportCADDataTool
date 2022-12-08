b13bd2b7d02f7f83b645c0b1709465eabe83ecdd
bacon <bacon0817.ix@gmail.com>
2022-12-08 13:18:29 +0900

    Zip

--- WindowsFormsApp1/CodeFile1.cs
+++ WindowsFormsApp1/CodeFile1.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System.IO;
+using System.Windows.Forms;
+using System;
+using WindowsFormsApp1;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using SolidworksAPIControl;
+using System.Windows.Forms.VisualStyles;
+using System.Diagnostics;
+using System.Linq;
+using System.IO.Compression;
+
+namespace WindowsFormsApp1
+{
+    public class Zipping
+    {
+        public void BandFileName(string[] FilePath,string ZipFolderPath)
+        {
+            string[] FileNameList= new string[FilePath.Length];
+
+            for(var i=0;i<FilePath.Length;i++)
+            {
+                FileNameList[i] = Path.GetFileNameWithoutExtension(@FilePath[i]);
+            }
+            FileNameList.Distinct();
+            
+            for(var i=0;i< FileNameList.Length;i++)
+            {
+                ZipFile
+        }
+    }
+}
